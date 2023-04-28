3f863032f4854bf202a1aafdef57bab6648d0956
racheta <surajshogun@gmail.com>
2023-04-29 02:21:48 +0530

    social links added

--- src/assets/fake-data/data-slider.js
+++ src/assets/fake-data/data-slider.js
@@ -6,35 +6,35 @@ import img1 from '../images/common/itemslider.png';
 const dataSlider = [
     {
         id: 1,
-        title: 'Welcome to the VIRTUAL PERADOX RACING WORLD',
+        title: 'Welcome to the Virtual Race of Peradox World',
         desc : 'The Ultimate Decentralized blockchain-based car racing game that allows you to earn native tokens and NFTs while you enjoy the thrill of the race.',
         bgImg: bgImg1,
         img : img1
     },
     {
         id: 2,
-        title: 'Welcome to the VIRTUAL PERADOX RACING WORLD',
+        title: 'Welcome to the Virtual Race of Peradox World',
         desc : 'The Ultimate Decentralized blockchain-based car racing game that allows you to earn native tokens and NFTs while you enjoy the thrill of the race.',
         bgImg: bgImg2,
         classAction: 'two'
     },
     {
         id: 3,
-        title: 'Welcome to the VIRTUAL PERADOX RACING WORLD',
+        title: 'Welcome to the Virtual Race of Peradox World',
         desc : 'The Ultimate Decentralized blockchain-based car racing game that allows you to earn native tokens and NFTs while you enjoy the thrill of the race.',
         bgImg: bgImg3,
         classAction: 'three'
     },
     {
         id: 4,
-        title: 'Welcome to the VIRTUAL PERADOX RACING WORLD',
+        title: 'Welcome to the Virtual Race of Peradox World',
         desc : 'The Ultimate Decentralized blockchain-based car racing game that allows you to earn native tokens and NFTs while you enjoy the thrill of the race.',
         bgImg: bgImg1,
         img : img1
     },
     {
         id: 5,
-        title: 'Welcome to the VIRTUAL PERADOX RACING WORLD',
+        title: 'Welcome to the Virtual Race of Peradox World',
         desc : 'The Ultimate Decentralized blockchain-based car racing game that allows you to earn native tokens and NFTs while you enjoy the thrill of the race.',
         bgImg: bgImg2,
         classAction: 'two'
